abd8e59bd81758a804556b183702cc7253f9e05e
rajayns42-design <rajayns42@gmail.com>
2026-01-30 00:16:49 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("ᴀɴꜱʜɪᴋᴀ ꭙ ᴍᴜꜱɪᴄ")
 
 # Your User ID.
 OWNER_ID = list(
